46770883997d0e436cecf071fe339b9627691ea5
Marc <RhinoDevel@users.noreply.github.com>
2021-09-15 16:36:00 +0200

    Initial skeleton.

---
+++ README.md
@@ -0,0 +1,3 @@
+# soundpet
+A tool to create music with rectangle waves of different frequencies, only.
+E.g. to mimic the sound of a Commodore PET/CBM computer of the early 1980's.
---
+++ main.js
@@ -0,0 +1,18 @@
+
+// (c) Marcel Timm, RhinoDevel, 2021
+
+/** To be executed as last JavaScript file on page load.
+ */
+(function() // IIFE
+{
+    'use strict';
+
+    var f = {};
+
+    f.onLoad = function()
+    {
+        // Add code here.
+    };
+
+    window.addEventListener('load', f.onLoad);
+}());
