a4c96d2d6620cc384b9fa367601c5d9a246ce699
Marc <RhinoDevel@users.noreply.github.com>
2021-09-15 21:19:43 +0200

    Global object and frequency player singleton.

--- main.js
+++ main.js
@@ -11,20 +11,9 @@
 
     f.onLoad = function()
     {
-        var ctx = new AudioContext(),//webkitAudioContext
-            osciParams = {
-                type: 'square',
-                channelCount: 1,
-                channelCountMode: 'explicit',
-                frequency: 440 // Hz
-                //detune
-                // ...
-            },
-            osci = new OscillatorNode(ctx, osciParams);
-            
-        osci.connect(ctx.destination);
-        osci.start();
-        osci.frequency.setValueAtTime(880, ctx.currentTime + 3);
+        soundpet.freqPlay.init();
+
+        soundpet.freqPlay.on(880);
     };
 
     window.addEventListener('load', f.onLoad);
---
+++ soundpet.js
@@ -0,0 +1,13 @@
+
+// (c) Marcel Timm, RhinoDevel, 2021
+
+/** ONLY global object for soundpet that will be augmented by initialization
+ *  code from other JavaScript files which will be loaded and executed AFTER
+ *  this file.
+ */
+var soundpet = (function() // IIFE
+{
+    'use strict';
+
+    return {};
+}());
