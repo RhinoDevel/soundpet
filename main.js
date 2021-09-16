2633b74aad21b21f547c103375b10a87d7b91f3b
Marc <RhinoDevel@users.noreply.github.com>
2021-09-16 21:51:59 +0200

    Piano function with two full octaves.

--- main.js
+++ main.js
@@ -11,6 +11,39 @@
 
     f.onLoad = function()
     {
+        var playing = null,
+            buttons = [
+                ['2', 'C', '#', 5],
+                ['3', 'D', '#', 5],
+                ['5', 'F', '#', 5],
+                ['6', 'G', '#', 5],
+                ['7', 'A', '#', 5],
+
+                ['q', 'C', '', 5],
+                ['w', 'D', '', 5],
+                ['e', 'E', '', 5],
+                ['r', 'F', '', 5],
+                ['t', 'G', '', 5],
+                ['z', 'A', '', 5],
+                ['u', 'B', '', 5],
+                ['i', 'C', '', 6],
+
+                ['s', 'C', '#', 4],
+                ['d', 'D', '#', 4],
+                ['g', 'F', '#', 4],
+                ['h', 'G', '#', 4],
+                ['j', 'A', '#', 4],
+
+                ['y', 'C', '', 4],
+                ['x', 'D', '', 4],
+                ['c', 'E', '', 4],
+                ['v', 'F', '', 4],
+                ['b', 'G', '', 4],
+                ['n', 'A', '', 4],
+                ['m', 'B', '', 4],
+                [',', 'C', '', 5]
+            ];
+
         soundpet.freqplay.init();
 
         soundpet.noteplay.init(
@@ -23,18 +56,29 @@
             {
                 onLoop: function(timestamp)
                 {
-                    if(soundpet.keyboard.isPressed('a'))
+                    // TODO: Stop unwanted "vibrato"!
+
+                    var i = -1;
+
+                    if(playing !== null)
                     {
-                        soundpet.noteplay.on('C', '', 4);
+                        if(!soundpet.keyboard.isPressed(playing))
+                        {
+                            soundpet.noteplay.off();
+                            playing = null;
+                        }
                     }
-                    if(soundpet.keyboard.isPressed('b'))
+
+                    for(i = 0;i < buttons.length; ++i)
                     {
-                        soundpet.noteplay.on('D', '', 4);
-                    }
-                    
-                    if(soundpet.keyboard.isPressed('c'))
-                    {
-                        soundpet.noteplay.off();
+                        if(soundpet.keyboard.isPressed(buttons[i][0]) 
+                            && playing !== buttons[i][0])
+                        {
+                            soundpet.noteplay.on(
+                                buttons[i][1], buttons[i][2], buttons[i][3]);
+                            playing = buttons[i][0];
+                            return;
+                        }
                     }
                 }
             });
