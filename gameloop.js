8d26d6c9371c6bb135a08009584eb761c22b77ef
Marc <RhinoDevel@users.noreply.github.com>
2021-12-23 21:59:37 +0100

    Comments.

--- gameloop.js
+++ gameloop.js
@@ -11,8 +11,10 @@
     var f = {}, v = {}, o = {};
 
     v.last_timestamp = 0.0;
-    v.step = 0.0; // ms
-    v.delta = 0.0;
+    v.step = 0.0; // ms (e.g. ~16.7ms for 60 FPS).
+    v.delta = 0.0; // Holds milliseconds that still need to be processed by
+                   // v.update(). Is not 0.0, if wanted FPS is not dividable
+                   // by .requestAnimationFrame()'s FPS without a remainder. 
 
     v.update = null;
     v.draw = null;
@@ -32,7 +34,7 @@
 
         do
         {
-            v.update();
+            v.update(); // Called once per step.
 
             v.delta -= v.step;
         }while(v.delta >= v.step);
